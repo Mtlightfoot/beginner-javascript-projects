943be4abf735f02056a460aa081a59af42cc222e
Mtlightfoot <M-lightfoot@hotmail.com>
2023-08-01 13:17:59 +0100

    Created button transitions and updated links

---
+++ colour-flipper/app.js
@@ -0,0 +1,7 @@
+const colours = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const colour = document.querySelector('.color');
+
+button.addEventListener('click', function(){
+    console.log(document.body);
+})
